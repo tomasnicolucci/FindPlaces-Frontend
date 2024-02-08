99130effd60e6b8c1822a2b21826bf14bbaa9a8f
tomasnicolucci <tomas.nicolucci@gmail.com>
2024-02-08 20:53:48 -0300

    Links

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Page from '../../components/Page/Page'
-
+import { Link } from "react-router-dom"
 const Home = () => {
   return (
     <Page>
       <div>Home</div>
+      <Link to="/Places">
+        <a className='btn btn-primary'>Lugares</a>
+      </Link>
     </Page>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import { Link } from "react-router-dom"
 const Navbar = () => {
     return(
         <nav className='navbar navbar-dark bg-dark'>
-            
-            <h2 style={{color: "white", marginLeft:"20px"}}>Inicio</h2>
-            
+            <Link to ="/">
+                <h2 style={{color: "white", marginLeft:"20px"}}>Find Places</h2>
+            </Link>
+            <Link to="/Places">
+                <h2>Places</h2>
+            </Link>
         </nav>
     );
 }
