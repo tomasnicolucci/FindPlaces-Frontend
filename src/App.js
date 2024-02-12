93cbca300bffa9842f77c0ec279e176abb67fa3b
tomasnicolucci <tomas.nicolucci@gmail.com>
2024-02-12 19:55:55 -0300

    New routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './Pages/Home/Home';
 import Places from './Pages/Places/Places';
 import Details from './Pages/Details/Details';
+import PlaceForm from './Pages/Places/PlaceForm';
 
 export default function App(){
   return (
@@ -12,6 +13,7 @@ export default function App(){
         <Route exact path="/" element={<Home></Home>}></Route>
         <Route exact path="/Places" element={<Places></Places>}></Route>
         <Route exact path="/place/:id" element={<Details></Details>}></Route>
+        <Route exact path="/Places/new" element={<PlaceForm></PlaceForm>}></Route>
       </Routes>  
     </Router>  
   );
--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { getPlaceById } from '../../Services/places.js'
+import Page from '../../components/Page/Page.jsx'
 
 const Details = () => {
   const [place, setPlace] = useState(null);
@@ -17,16 +18,16 @@ const Details = () => {
     });
   }, [id]);
   return (
-    <>
-    <div>Details</div>
-    {place ? (
-      <div>
-        {place.name}
-      </div>
-    ) : (
-      <h2>Cargando...</h2>
-    )}
-    </>
+    <Page>
+      <div>Details</div>
+      {place ? (
+        <div>
+          {place.name}
+        </div>
+      ) : (
+        <h2>Cargando...</h2>
+      )}
+    </Page>
   )
 }
 
