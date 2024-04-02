afdafdc3a44069d26fd4142f965d5f5eff9c87f0
tomasnicolucci <tomas.nicolucci@gmail.com>
2024-04-01 21:12:30 -0300

    New styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,20 +5,20 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return(
         <nav className='navbar navbar-dark bg-dark'>
-            <Link to ="/">
-                <h2 style={{color: "white", marginLeft:"20px"}}>Find Places</h2>
+            <Link to ="/" className="icono">
+                <h2>Find Places</h2>
             </Link>
             <Link to="/Places" className="icono">
-                <h2>Lugares</h2>
+                <h3>Lugares</h3>
             </Link>
             <Link to="/login" className="icono">
-                <h2>Login</h2>
+                <h3>Login</h3>
             </Link>
-            <Link to="/users/favorites">
-                <h2>Favs</h2>
+            <Link to="/users/favorites" className="icono">
+                <h3>Favs</h3>
             </Link>
-            <Link to="/users/visited">
-                <h2>Visited</h2>
+            <Link to="/users/visited" className="icono">
+                <h3>Visited</h3>
             </Link>
         </nav>
     );
