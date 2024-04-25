d40a4e6e562d33d9d6b59480767abfc197b4a4bc
tomasnicolucci <tomas.nicolucci@gmail.com>
2024-04-25 18:56:32 -0300

    general changes

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,9 +3,37 @@ import React, { useState } from 'react'
 const Search = ({ places }) => {
 
     const [searchInput, setSearchInput] = useState("");
+    const [filteredPlaces, setFilteredPlaces] = useState("");
+
+    const handleChange = (value) => {
+        setSearchInput(value);
+        const fltrdPlaces = places.filter((place) => {
+            return (
+                value &&
+                place &&
+                place.name.value &&
+                place.name.value.toLowerCase().includes(value)
+            )
+        })
+        setFilteredPlaces(fltrdPlaces);
+    };
 
     return (
-        <div>Search</div>
+        places ? 
+        <>
+            <input type="text" placeholder="Busca un lugar" value={searchInput} onChange={(e) => handleChange(e.target.value)} className='searchInput'/>
+            <div>
+            
+                {
+                (searchInput.length > 0) ?
+                    filteredPlaces.map((place) => {
+                        return place
+                    })
+                : <><p>No se ha encontrado</p></>
+                }
+            </div></>
+        :
+        <></>
     )
 }
 
