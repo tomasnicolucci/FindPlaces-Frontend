6bc53ef23d526cb1901d9d76da5cb3139b081e0c
tomasnicolucci <tomas.nicolucci@gmail.com>
2024-05-28 16:08:47 -0300

    New functionality and styles in favorites and visited

--- src/Pages/Places/Places.jsx
+++ src/Pages/Places/Places.jsx
@@ -1,14 +1,10 @@
-import React, { useContext, useEffect, useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Places.css';
 import Place from '../../components/Places/Place.jsx'
-// import { getPlaces, deletePlace } from '../../Services/places.js'
-import { addFavorite, addVisited, getAllFavorites, getAllVisited } from '../../Services/users.js'
+import { /*addFavorite, addVisited,*/ getAllFavorites, getAllVisited } from '../../Services/users.js'
 import { PlacesContext } from '../../Context/PlacesContext.jsx'
 import Page from '../../components/Page/Page.jsx'
 import { Link } from 'react-router-dom'
-// import { faHeart } from '@fortawesome/free-solid-svg-icons';
-// import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Places = () => {
   const { places } = useContext(PlacesContext);
@@ -18,35 +14,37 @@ const Places = () => {
   const [update, setUpdate] = useState(false);
   const request = {
     method: 'GET',
-    headers: {'Content-Type': 'application/json',
-    'Authorization': `Bearer ${localStorage.getItem('token')}`}
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
   }
 
   useEffect(() => {
     const fetchFavorites = async () => {
-        try {
-            const favs = await getAllFavorites(request);
-            console.log('Favoritos devueltos:', favs);  // Añadir este log para depuración
-            setFavorites(favs);
-            setUpdate(true);
-        } catch (error) {
-            console.error('Error al obtener favoritos:', error);
-            setFavorites([]);  // Asegúrate de que `favorites` sea un array en caso de error
-        }
+      try {
+        const favs = await getAllFavorites(request);
+        console.log('Favoritos devueltos:', favs);
+        setFavorites(favs);
+        setUpdate(true);
+      } catch (error) {
+        console.error('Error al obtener favoritos:', error);
+        setFavorites([]);
+      }
     };
 
     const fetchVisited = async () => {
-      try{
+      try {
         const vstd = await getAllVisited(request);
         setVisited(vstd)
-      } catch(error){
+      } catch (error) {
         setVisited([]);
       }
     };
 
     fetchFavorites();
     fetchVisited();
-}, [update]);
+  }, [update]);
 
   // const favOnClick = async (id) =>{
   //   handleButtonClick();
@@ -78,18 +76,6 @@ const Places = () => {
   //     setIsAnimating(false);
   //   }, 1000);
   // }
-  
-  // const handleDelete = async (id) => {
-  //   const request = {
-  //     method: 'DELETE'
-  //   }
-  //   try{
-  //     await deletePlace(id, request);
-  //     setUpdate(true);
-  //   } catch(error){
-  //     console.log(error);
-  //   }
-  // }
 
   return (
     <Page>
@@ -103,31 +89,17 @@ const Places = () => {
       <Link to="/users/visited">
         <button className="btn btn-primary btn-custom">Visitados</button>
       </Link>
-      <div className='row row-custom' style={{marginTop: '20px', marginLeft: '20px'}}>
+      <div className='row row-custom' style={{ marginTop: '20px', marginLeft: '20px' }}>
         {places !== null ? (
           places.map((p) => {
-            const esFavorito = favorites.includes(p._id);
-            const fav = esFavorito ? true : false;
-            console.log(fav);
-            return <Place place={p} favorito={fav} key={p._id} />;
-
+            const esFavorito = favorites.some(f => f._id === p._id);
+            const esVisitado = visited.some(v => v._id === p._id);
+            // console.log(`Lugar: ${p.name}, Favorito con esFavorito: ${esFavorito}`);
+            return <Place place={p} favorito={esFavorito} visitado={esVisitado} key={p._id} />;
           })
-            // <div key={p._id} className='col-md-4 mb-4'>
-            //   <div className="card bg-transparent card-custom">
-            //     <div className="card-body place">
-            //       <h5 className="card-title">{p.name}</h5>
-            //       <FontAwesomeIcon className="btnAdd col-1" icon={faHeart} onClick={() => favOnClick(p._id)} />
-            //       <FontAwesomeIcon className="btnAdd col-2" icon={faSquareCheck} onClick={() => visitedOnClick(p._id)}/>
-            //       <p className="card-text">{p.description}</p>
-            //       <Link to={`/place/${p._id}`}>
-            //         <button className="btn btn-primary">Ver mas</button>
-            //       </Link>
-            //       <button onClick={() => handleDelete(p._id)} className="btn btn-danger btn-custom">Eliminar</button>
-            //     </div>
-            //   </div>
-            // </div>
-          )
-         : ('No hay lugares para mostrar')}
+        )
+        :
+        ('No hay lugares para mostrar')}
       </div>
     </Page>
   )
--- src/components/Places/Place.jsx
+++ src/components/Places/Place.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './Place.css'
 import { Link } from 'react-router-dom'
 import { deletePlace } from '../../Services/places.js'
@@ -7,120 +7,82 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Place = ({place, fav}) => {
-    // const {_id, name, description} = props.place;
-    const [update, setUpdate] = useState(false)
-    const [isAnimating, setIsAnimating] = useState(false);
-    const request = {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json',
-      'Authorization': `Bearer ${localStorage.getItem('token')}`}
+const Place = ({ place, favorito, visitado }) => {
+  // console.log('Place component:', { place, favorito }); 
+  const [update, setUpdate] = useState(false)
+  const [isAnimating, setIsAnimating] = useState(false);
+  const request = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
-    // const [places, setPlaces] = useState(null)
+  }
 
-    // useEffect( () => {
-    //   getPlaces(setPlaces);
-    //   setUpdate(false);
-    // }, [update])
+  const favOnClick = async (id) => {
+    handleButtonClick();
+    if (!favorito) {
+      try {
+        await addFavorite(id, request);
+        setUpdate(true);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
 
-    // const handleDelete = async (id) => {
-    //     const request = {
-    //       method: 'DELETE'
-    //     }
-    //     try{
-    //       await deletePlace(id, request);
-    //       setUpdate(true);
-    //     } catch(error){
-    //       console.log(error);
-    //     }
-    //   }
-    
-    //   const handleFavorite = async (id) => {
-    //     const storedToken = localStorage.getItem('token');
-    //     const request = {
-    //       method: 'POST',
-    //       headers: {'Content-Type': 'application/json',
-    //       'Authorization': `Bearer ${storedToken}`}
-    //     }
-    //     try{
-    //       await addFavorite(id, request);
-    //       setUpdate(true);
-    //     }catch(error){
-    //       console.log(error);
-    //     }
-    //   }
-    
-    //   const handleVisited = async (id) => {
-    //     const storedToken = localStorage.getItem('token');
-    //     const request = {
-    //       method: 'POST',
-    //       headers: {'Content-Type': 'application/json',
-    //       'Authorization': `Bearer ${storedToken}`}
-    //     }
-    //     try{
-    //       await addVisited(id, request);
-    //       setUpdate(true);
-    //     }catch(error){
-    //       console.log(error);
-    //     }
-    //   }
-      const favOnClick = async (id) =>{
-          handleButtonClick();
-          if(false){
-            //removeFavorite(id)
-          }else{
-            try{
-              await addFavorite(id, request);
-              setUpdate(true);
-            }catch(error){
-              console.log(error);
-            }
-          }    
-        }
+    }
+  }
 
-        const visitedOnClick = async (id) => {
-          handleButtonClick();
-          try{
-            await addVisited(id, request);
-            setUpdate(true);
-          }catch(error){
-            console.log(error);
-          }
-        }
+  const visitedOnClick = async (id) => {
+    handleButtonClick();
+    if (!visitado) {
+      try {
+        await addVisited(id, request);
+        setUpdate(true);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
 
-        const handleButtonClick = () => {
-          setIsAnimating(true);
-          setTimeout(() => {
-            setIsAnimating(false);
-          }, 1000);
-        }
-        
-        const handleDelete = async (id) => {
-          const request = {
-            method: 'DELETE'
-          }
-          try{
-            await deletePlace(id, request);
-            setUpdate(true);
-          } catch(error){
-            console.log(error);
-          }
-        }
+    }
+  }
+
+  const handleButtonClick = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 1000);
+  }
+
+  const handleDelete = async (id) => {
+    const request = {
+      method: 'DELETE'
+    }
+    try {
+      await deletePlace(id, request);
+      setUpdate(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="card" style={{width: "20rem"}}>
-        <div className="card-body">
-        <h5 className="card-title">{place.name}</h5>
-        <p className="card-text">{place.description}</p>
-        <FontAwesomeIcon className={`${fav === true ? "corazon col-1" : "btnAdd col-2"}`} icon={faHeart} onClick={() => favOnClick(place._id)} />       
-        <FontAwesomeIcon className="btnAdd col-2" icon={faSquareCheck} onClick={() => visitedOnClick(place._id)}/>      
-        <Link to={`/place/${place._id}`}>
+    <div key={place._id} className='col-md-4 mb-4'>
+      <div className="card bg-transparent card-custom">
+        <div className="card-body place">
+          <div className="titleContainer">
+            <h5 className="card-title">{place.name}</h5>
+            <FontAwesomeIcon className={`${favorito ? "btnAdd corazon" : "btnAdd"}`} icon={faHeart} onClick={() => favOnClick(place._id)} />
+            <FontAwesomeIcon className={`${visitado ? "btnAdd check col-2" : "btnAdd col-2"}`} icon={faSquareCheck} onClick={() => visitedOnClick(place._id)} />
+          </div>
+          <p className="card-text">{place.description}</p>
+          <Link to={`/place/${place._id}`}>
             <button className="btn btn-primary">Ver mas</button>
-        </Link>
-        <button onClick={() => handleDelete(place._id)} className="btn btn-danger">Eliminar</button>
-        <div>{place.favorito}</div>
+          </Link>
+          <button onClick={() => handleDelete(place._id)} className="btn btn-danger btn-custom">Eliminar</button>
         </div>
+      </div>
     </div>
-    
   )
 }
 
